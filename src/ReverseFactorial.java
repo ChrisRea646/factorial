bdddb4065af09c908e7551f1ff51bed30349ffeb
Tyler Eddy <tylereddy96@gmail.com>
2020-02-05 13:28:19 +0000

    Added funtionallity

---
+++ src/ReverseFactorial.java
@@ -0,0 +1,16 @@
+
+public class ReverseFactorial {
+	
+	int divider = 2;
+	public void reverse(double num) {
+	
+		while(num !=1 && num >1) {
+			num = num/divider;
+			System.out.println(num);
+			
+		}
+		
+		
+	}
+
+}
--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,8 @@ public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ReverseFactorial rf = new ReverseFactorial();
+		rf.reverse(150);
 	}
 
 }
